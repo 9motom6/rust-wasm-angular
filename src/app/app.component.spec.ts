a59a8396bfdcfb6053840edf52c5dd25b82691c4
9motom6 <9motom6@gmail.com>
2020-09-21 17:02:03 +0200

    Call wasm from Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('rust-wasm-angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('rust-wasm-angular app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rust-wasm-angular';
+
+  ngOnInit(): void {
+    const rust = import('../../wasm/pkg');
+
+    rust.then(m => m.greet()).catch(console.error);
+  }
 }
